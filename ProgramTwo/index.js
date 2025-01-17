fd39acb5bf287150223f39826fa123b13df3d1f9
gpunit2417 <punitgoyal106@gmail.com>
2025-01-17 16:01:08 +0530

    new program

---
+++ ProgramTwo/index.js
@@ -0,0 +1,10 @@
+// function sum(a, b) {
+//     return a+b
+// }
+// console.log(sum(5, "vinod"));   //Result is 5vinod because type is not specified and logically it should throw error
+
+
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 10));
---
+++ ProgramTwo/index.ts
@@ -0,0 +1,16 @@
+//Write a program to add two numbers without error handling
+
+// function sum(a, b) {
+//     return a+b
+// }
+
+// console.log(sum(5, "vinod"));   //Result is 5vinod because type is not specified and logically it should throw error
+
+
+
+//Write a program to add two numbers with error handling
+
+function sum(a:number, b:number):number {   //explicitly specified datatype, so error handling
+    return a+b
+}
+console.log(sum(5, 10));    //Result is 15
