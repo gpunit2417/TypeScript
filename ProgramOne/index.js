f70ce698bec1458744c68f3d69b89a19426f78c5
gpunit2417 <punitgoyal106@gmail.com>
2025-01-17 15:46:00 +0530

    moved into ProgramOne folder

---
+++ ProgramOne/index.js
@@ -0,0 +1,5 @@
+console.log("This is Punit learning typescript");
+var num = 5;
+// num = "Vinod"    //This is an error because initially num is assigned to a number and we cannot assign it to any other type of data
+num = 55;
+console.log(num);
---
+++ ProgramOne/index.ts
@@ -0,0 +1,5 @@
+console.log("This is Punit learning typescript");
+let num = 5
+// let num:number = 5   //another method to declare variables
+// num = "Vinod"    //This is an error because initially num is assigned to a number and we cannot assign it to any other type of data
+// num = 55
