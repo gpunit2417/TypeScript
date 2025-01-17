a8cbb767087b9de20f304b9e2afef079f2124997
gpunit2417 <punitgoyal106@gmail.com>
2025-01-17 16:21:47 +0530

    handled creation of js file when error is in ts file

--- ProgramTwo/index.js
+++ ProgramTwo/index.js
@@ -13,3 +13,16 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(5, 10));
+//TS configuration file
+// - This file contains many in-built commands
+// we will ensure if our ts file contains any error, then on executing it no js file creates.
+//for this, we will run *tsc --init* command which will create a tsconfig file
+//After that, search for emiton in tsconfig.json file and uncomment it.
+// function sum(a:number, b:number):number {   //explicitly specified datatype, so error handling
+//     return a+b
+// }
+// console.log(sum(5, "vinod"));    //Result is 15
+//the above line contains a string as an argument, but required is a number...
+//on running tsc index.ts command, an error is shown but index.js file is created.
+//so to prevent creation of index.js file, we will run command *tsc --noEmitOnError index.ts*
+//On running above command, no index.js file is constructed.
