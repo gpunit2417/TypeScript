6dc2602aa431c94d96f5091d88b4a946870a458b
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 10:10:03 +0530

    unknown datatype

--- ProgramThree/any_unknown.js
+++ ProgramThree/any_unknown.js
@@ -9,3 +9,16 @@ var number = 5;
 var number1 = 6;
 number1 = "punit"; //no error as any particular datatype is not specified 
 console.log(number1);
+// Unknown 
+//safer alternative to any because it still enforces type checking and type safety
+//variables of type unknown can hold value of any datatype, but oyu must perform type checks or type assertions before using them in specific way.
+var num1;
+num1 = 5;
+num1 = "punit";
+num1 = true;
+if (typeof num1 === "number") {
+    console.log(num1 + 5);
+}
+else if (typeof num1 === "boolean") {
+    console.log(num1);
+}
--- ProgramThree/any_unknown.ts
+++ ProgramThree/any_unknown.ts
@@ -14,3 +14,22 @@ let number = 5
 let number1 : any = 6
 number1 = "punit"      //no error as any particular datatype is not specified 
 console.log(number1);
+
+
+
+// Unknown 
+//safer alternative to any because it still enforces type checking and type safety
+//variables of type unknown can hold value of any datatype, but oyu must perform type checks or type assertions before using them in specific way.
+
+
+let num1: unknown
+num1 = 5
+num1 = "punit"
+num1 = true
+
+if(typeof num1 === "number"){
+    console.log(num1 + 5);
+}
+else if(typeof num1 === "boolean"){
+    console.log(num1);
+}
