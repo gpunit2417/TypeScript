38a1f31ef4cb1442abbd34a7a933b7675ef3de7b
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 09:26:47 +0530

    corresponding js file for type_annotation.ts file

--- ProgramThree/type_annotation.js
+++ ProgramThree/type_annotation.js
@@ -1,7 +1,8 @@
+"use strict";
 // * type annotation is a way of explicitly specifying the type of a variable, function parameter or a function return value.
 // let myNumber: number = 10
 // myNumber = '5'   //error because type is number.
-var myNumber1 = 10;
+let myNumber1 = 10;
 // myNumber1 = '5'     //no error because no particular type is specified.
 console.log(myNumber1.toString());
 //Advantages
@@ -9,15 +10,15 @@ console.log(myNumber1.toString());
 //to know what operations can be performed on the variable.
 //type annotations are expressed by using a colon followed by a desired type
 //Number type
-var myFav = 5;
-var myAge = 29;
-var pi = 3.14;
-var myNeg = -5;
+let myFav = 5;
+let myAge = 29;
+let pi = 3.14;
+let myNeg = -5;
 // let invalidResult: number = myAge + "years"
 // console.log(invalidResult)
 // let invalidResult: number = "123"
 // let computedValue: number = Math.sqrt(16)   //valid result
-var nanValue = NaN;
+let nanValue = NaN;
 console.log(nanValue);
 //String type
 // let myFirstName:string = "PUNIT"
@@ -26,24 +27,24 @@ console.log(nanValue);
 // console.log(myFullName)
 // let sentence: string = "Learning typescript from youtube"
 // console.log(sentence.length)
-var text = "need something badly";
-var uppercase = text.toUpperCase();
-var lowercase = text.toLowerCase();
+let text = "need something badly";
+let uppercase = text.toUpperCase();
+let lowercase = text.toLowerCase();
 // let uppercase:string = text.toLocaleUpperCase();
 // let lowercase:string = text.toLocaleLowerCase()
 console.log(uppercase); //same output for both
 console.log(lowercase); //same output for both
 // 1: Substring:
-var longText = "This is a long sentence.";
-var shortText = longText.substring(0, 10);
+let longText = "This is a long sentence.";
+let shortText = longText.substring(0, 10);
 console.log(shortText);
 // 2: String Comparison:
-var str1 = "Hello";
-var str2 = "World";
-var areEqual = str1 === str2;
+let str1 = "Hello";
+let str2 = "World";
+let areEqual = str1 === str2;
 console.log(areEqual);
 // 3: String Template:
-var product = "Phone";
-var price = 500;
-var message = "The product ".concat(product, " is priced at ").concat(price, " dollars.");
+let product = "Phone";
+let price = 500;
+let message = `The product ${product} is priced at ${price} dollars.`;
 console.log(message);
