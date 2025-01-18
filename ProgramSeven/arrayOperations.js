0660172e9604d9b2b49957209cc79ed880be8e7d
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 20:36:11 +0530

    iteration using for loop

--- ProgramSeven/arrayOperations.js
+++ ProgramSeven/arrayOperations.js
@@ -1,13 +1,18 @@
 var arr1 = ["Punit", "Sakshi", "Atul", "Monika"];
-var newArray = arr1.push("Raman");
-console.log(newArray);
-console.log(arr1);
-var removeArray = arr1.pop();
-console.log(removeArray);
-console.log(arr1);
-var anotherArray = arr1.unshift("Raman");
-console.log(anotherArray);
-console.log(arr1);
-var removeFirst = arr1.shift();
-console.log(removeFirst);
-console.log(arr1);
+// const newArray = arr1.push("Raman")
+// console.log(newArray);
+// console.log(arr1);
+// const removeArray = arr1.pop();
+// console.log(removeArray);
+// console.log(arr1);
+// const anotherArray = arr1.unshift("Raman")
+// console.log(anotherArray);
+// console.log(arr1);
+// const removeFirst = arr1.shift();
+// console.log(removeFirst);
+// console.log(arr1);
+//Access elements using for loops
+//1. for loop
+for (var i = 0; i < arr1.length; i++) {
+    console.log(i + arr1[i]);
+}
--- ProgramSeven/arrayOperations.ts
+++ ProgramSeven/arrayOperations.ts
@@ -1,18 +1,26 @@
 const arr1 = ["Punit", "Sakshi", "Atul", "Monika"]
 
-const newArray = arr1.push("Raman")
-console.log(newArray);
-console.log(arr1);
+// const newArray = arr1.push("Raman")
+// console.log(newArray);
+// console.log(arr1);
 
-const removeArray = arr1.pop();
-console.log(removeArray);
-console.log(arr1);
+// const removeArray = arr1.pop();
+// console.log(removeArray);
+// console.log(arr1);
 
-const anotherArray = arr1.unshift("Raman")
-console.log(anotherArray);
-console.log(arr1);
+// const anotherArray = arr1.unshift("Raman")
+// console.log(anotherArray);
+// console.log(arr1);
 
-const removeFirst = arr1.shift();
-console.log(removeFirst);
-console.log(arr1);
+// const removeFirst = arr1.shift();
+// console.log(removeFirst);
+// console.log(arr1);
 
+
+//Access elements using for loops
+
+//1. for loop
+
+for(let i=0; i<arr1.length; i++){
+    console.log(i + arr1[i]);
+}
