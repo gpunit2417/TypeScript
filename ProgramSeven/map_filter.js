7bf4d3004edbe77ea9a780fb42b210e6e3d004fe
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 21:13:28 +0530

    array coding questions

--- ProgramSeven/map_filter.js
+++ ProgramSeven/map_filter.js
@@ -1,11 +1,31 @@
 //map operation
-var array = [1, 2, 3, 4, 5];
-var doubleArray = array.map(function (curElement) { return curElement * 2; });
-console.log(doubleArray);
-var numberToString = array.map(function (curVal) { return curVal.toString(); });
-console.log(numberToString);
-//filter operation
-var greaterThanThree = array.filter(function (current) { return current > 3; });
-console.log(greaterThanThree);
-var divisibleByTwo = array.filter(function (currentValue) { return currentValue % 2 == 0; });
-console.log(divisibleByTwo);
+// const array = [1, 2, 3, 4, 5]
+// const doubleArray = array.map((curElement) => curElement * 2)
+// console.log(doubleArray);
+// const numberToString = array.map((curVal: number) => curVal.toString())
+// console.log(numberToString);
+// //filter operation
+// const greaterThanThree = array.filter((current: number) => current > 3)
+// console.log(greaterThanThree);
+// const divisibleByTwo = array.filter((currentValue: number) => currentValue % 2 == 0)
+// console.log(divisibleByTwo);
+//calculate Average of numbers
+var calculateAverage = function (arr, page) {
+    if (page === void 0) { page = 10; }
+    var averageNum = arr.reduce(function (initialValue, currentValue) {
+        return initialValue + currentValue * page;
+    }, 0);
+    console.log(averageNum / arr.length);
+};
+calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+//find max number
+var findMaxValue1 = function (numbers) {
+    var maximumValue = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        if (maximumValue < numbers[i]) {
+            maximumValue = numbers[i];
+        }
+    }
+    return maximumValue;
+};
+console.log(findMaxValue1([4, 2, 42, 2, 3]));
--- ProgramSeven/map_filter.ts
+++ ProgramSeven/map_filter.ts
@@ -1,19 +1,47 @@
 //map operation
 
-const array = [1, 2, 3, 4, 5]
-const doubleArray = array.map((curElement) => curElement * 2)
-console.log(doubleArray);
+// const array = [1, 2, 3, 4, 5]
+// const doubleArray = array.map((curElement) => curElement * 2)
+// console.log(doubleArray);
 
 
-const numberToString = array.map((curVal: number) => curVal.toString())
-console.log(numberToString);
+// const numberToString = array.map((curVal: number) => curVal.toString())
+// console.log(numberToString);
 
 
-//filter operation
+// //filter operation
 
-const greaterThanThree = array.filter((current: number) => current > 3)
-console.log(greaterThanThree);
+// const greaterThanThree = array.filter((current: number) => current > 3)
+// console.log(greaterThanThree);
 
 
-const divisibleByTwo = array.filter((currentValue: number) => currentValue % 2 == 0)
-console.log(divisibleByTwo);
+// const divisibleByTwo = array.filter((currentValue: number) => currentValue % 2 == 0)
+// console.log(divisibleByTwo);
+
+
+
+//calculate Average of numbers
+
+const calculateAverage = (arr:number[], page=10) => {
+    let averageNum = arr.reduce((initialValue, currentValue) => {
+        return initialValue + currentValue * page
+    }, 0)
+    console.log(averageNum/arr.length)
+}
+calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
+
+//find max number
+
+const findMaxValue1 = (numbers:any):number =>{
+    let maximumValue:number = 0
+    for(let i:number=0; i<numbers.length; i++){
+        if (maximumValue < numbers[i]){
+            maximumValue = numbers[i];
+        }
+    }
+    return maximumValue
+
+}
+
+console.log(findMaxValue1([4, 2, 42, 2, 3]));
