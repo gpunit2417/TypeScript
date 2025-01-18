03ff5289df8aef9407a2a3e735c98c55c78516d7
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 09:50:00 +0530

    bigint data type

--- ProgramThree/bool_bigint.js
+++ ProgramThree/bool_bigint.js
@@ -1,14 +1,29 @@
-"use strict";
 // Boolean type
-let isNumber = true;
-let isDivisibleByFour = false;
+var isNumber = true;
+var isDivisibleByFour = false;
 //function to check for odd or even
 function isEven(a) {
     return a % 2 == 0;
 }
-console.log(isEven(5));
+// console.log(isEven(5));
 //function to check for a number divisible by 4 and 8
 function isDivisibleByFourAndEight(a) {
     return (a % 4 == 0 && a % 8 == 0);
 }
-console.log(isDivisibleByFourAndEight(81));
+// console.log(isDivisibleByFourAndEight(81));
+// BigInt
+var maxValue = Number.MAX_SAFE_INTEGER;
+console.log(maxValue);
+var bigValue = 9007199254740991n;
+console.log(bigValue);
+var anotherBigValue = BigInt("9007199254740991");
+console.log(anotherBigValue);
+//@ts-ignore
+var sum = bigValue + anotherBigValue;
+console.log(sum);
+var difference = bigValue - anotherBigValue;
+console.log(difference);
+var multiply = bigValue * anotherBigValue;
+console.log(multiply);
+var divide = bigValue / anotherBigValue;
+console.log(divide);
--- ProgramThree/bool_bigint.ts
+++ ProgramThree/bool_bigint.ts
@@ -8,7 +8,7 @@ function isEven(a:number): boolean{
     return a%2 == 0
 }
 
-console.log(isEven(5));
+// console.log(isEven(5));
 
 
 //function to check for a number divisible by 4 and 8
@@ -17,5 +17,33 @@ function isDivisibleByFourAndEight(a:number): boolean{
     return (a%4 == 0 && a%8 == 0)
 }
 
-console.log(isDivisibleByFourAndEight(81));
+// console.log(isDivisibleByFourAndEight(81));
 
+
+
+// BigInt
+
+let maxValue = Number.MAX_SAFE_INTEGER
+console.log(maxValue);
+
+let bigValue: bigint = 9007199254740991n
+console.log(bigValue);
+
+let anotherBigValue = BigInt("9007199254740991")
+console.log(anotherBigValue);
+
+//@ts-ignore
+let sum = bigValue + anotherBigValue
+console.log(sum);
+
+
+let difference = bigValue - anotherBigValue
+console.log(difference);
+
+
+let multiply = bigValue * anotherBigValue
+console.log(multiply);
+
+
+let divide = bigValue / anotherBigValue
+console.log(divide);
