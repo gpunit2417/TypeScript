aff4e6d8389a37127222c6bf7cfec002c5f3c4ac
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 10:31:51 +0530

    functions in various ways

---
+++ ProgramFour/intro_function.js
@@ -0,0 +1,26 @@
+// function greet(name: string, id: number){
+//     console.log(`Welcome, ${name}. Your id is ${id}`);
+// }
+// greet("Punit", 1)
+// function greet(name: string, id: number){
+//     return `Welcome, ${name}. Your id is ${id}`
+// }
+// console.log(greet("Punit", 1))
+// function greet(name: string, id: number): string{
+//     return `Welcome, ${name}. Your id is ${id}`
+// }
+// console.log(greet("Punit", 1))
+// function greet(name: string, id: number): void{
+//     console.log(`Welcome, ${name}. Your id is ${id}`);
+// }
+// greet("Punit", 1)
+//fat arrow functions
+// const greet = (name: string, id: number) => {
+//     console.log(`Welcome, ${name}. Your id is ${id}`);
+// }
+// greet("Punit", 1)
+var greet = function (name, id) {
+    return "Welcome, ".concat(name, ". Your id is ").concat(id);
+};
+var myGreet = greet("Monika", 2);
+console.log(myGreet);
