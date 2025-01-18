f12b8307eeebe026b2fb9d232cf7f5feeaae3108
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 21:18:22 +0530

    map practice questions

--- ProgramSeven/map_filter.js
+++ ProgramSeven/map_filter.js
@@ -10,22 +10,29 @@
 // const divisibleByTwo = array.filter((currentValue: number) => currentValue % 2 == 0)
 // console.log(divisibleByTwo);
 //calculate Average of numbers
-var calculateAverage = function (arr, page) {
-    if (page === void 0) { page = 10; }
-    var averageNum = arr.reduce(function (initialValue, currentValue) {
-        return initialValue + currentValue * page;
-    }, 0);
-    console.log(averageNum / arr.length);
-};
-calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+// const calculateAverage = (arr:number[], page=10) => {
+//     let averageNum = arr.reduce((initialValue, currentValue) => {
+//         return initialValue + currentValue * page
+//     }, 0)
+//     console.log(averageNum/arr.length)
+// }
+// calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 //find max number
-var findMaxValue1 = function (numbers) {
-    var maximumValue = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        if (maximumValue < numbers[i]) {
-            maximumValue = numbers[i];
-        }
-    }
-    return maximumValue;
-};
-console.log(findMaxValue1([4, 2, 42, 2, 3]));
+// const findMaxValue1 = (numbers:any):number =>{
+//     let maximumValue:number = 0
+//     for(let i:number=0; i<numbers.length; i++){
+//         if (maximumValue < numbers[i]){
+//             maximumValue = numbers[i];
+//         }
+//     }
+//     return maximumValue
+// }
+// console.log(findMaxValue1([4, 2, 42, 2, 3]));
+//practice question for map
+//1. uppercase of a string array containing names
+var namedArray = ["Punit", "goyal", "purohit"];
+var uppercasedArray = namedArray.map(function (curr) { return curr.toUpperCase(); });
+console.log(uppercasedArray);
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var squaredArray = numberArray.map(function (curValue) { return curValue * curValue; });
+console.log(squaredArray);
--- ProgramSeven/map_filter.ts
+++ ProgramSeven/map_filter.ts
@@ -22,26 +22,39 @@
 
 //calculate Average of numbers
 
-const calculateAverage = (arr:number[], page=10) => {
-    let averageNum = arr.reduce((initialValue, currentValue) => {
-        return initialValue + currentValue * page
-    }, 0)
-    console.log(averageNum/arr.length)
-}
-calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+// const calculateAverage = (arr:number[], page=10) => {
+//     let averageNum = arr.reduce((initialValue, currentValue) => {
+//         return initialValue + currentValue * page
+//     }, 0)
+//     console.log(averageNum/arr.length)
+// }
+// calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 
 
 //find max number
 
-const findMaxValue1 = (numbers:any):number =>{
-    let maximumValue:number = 0
-    for(let i:number=0; i<numbers.length; i++){
-        if (maximumValue < numbers[i]){
-            maximumValue = numbers[i];
-        }
-    }
-    return maximumValue
+// const findMaxValue1 = (numbers:any):number =>{
+//     let maximumValue:number = 0
+//     for(let i:number=0; i<numbers.length; i++){
+//         if (maximumValue < numbers[i]){
+//             maximumValue = numbers[i];
+//         }
+//     }
+//     return maximumValue
 
-}
+// }
 
-console.log(findMaxValue1([4, 2, 42, 2, 3]));
+// console.log(findMaxValue1([4, 2, 42, 2, 3]));
+
+
+//practice question for map
+
+//1. uppercase of a string array containing names
+// const namedArray = ["Punit", "goyal", "purohit"]
+// const uppercasedArray = namedArray.map((curr: string) => curr.toUpperCase())
+// console.log(uppercasedArray);
+
+
+// const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const squaredArray = numberArray.map((curValue: number) => curValue * curValue)
+// console.log(squaredArray);
