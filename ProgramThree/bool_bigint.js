ea84e27aac8a3fbd2e6fc5030790cdfc37cda90a
gpunit2417 <punitgoyal106@gmail.com>
2025-01-18 09:24:38 +0530

    boolean data type

---
+++ ProgramThree/bool_bigint.js
@@ -0,0 +1,9 @@
+"use strict";
+// Boolean type
+let isNumber = true;
+let isDivisibleByFour = false;
+//function to check for odd or even
+function isEven(a) {
+    return a % 2 == 0;
+}
+console.log(isEven(5));
---
+++ ProgramThree/bool_bigint.ts
@@ -0,0 +1,11 @@
+// Boolean type
+let isNumber : boolean = true
+let isDivisibleByFour: boolean = false
+
+//function to check for odd or even
+
+function isEven(a:number): boolean{
+    return a%2 == 0
+}
+
+console.log(isEven(5));
