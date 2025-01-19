0c5742fd9e8318d69875bec2955a782a4377fb72
gpunit2417 <punitgoyal106@gmail.com>
2025-01-19 17:40:27 +0530

    practice question one done

--- ProgramTwelve/Union.js
+++ ProgramTwelve/Union.js
@@ -1,5 +1,21 @@
+//union in TS is implemented using pipe(|) operation. It is used to combine different values
+// const inputValue = (userValue: string | number | boolean) => {
+//     console.log(userValue);
+// }
+// inputValue("Punit")
+// inputValue(2417)
+// inputValue(true)
+// Practice Question
 var inputValue = function (userValue) {
-    console.log(userValue);
+    if (typeof userValue === "string") {
+        console.log(userValue.toUpperCase());
+    }
+    else if (typeof userValue === "number") {
+        console.log(userValue * 2);
+    }
+    else if (typeof userValue === "boolean") {
+        console.log(userValue);
+    }
 };
 inputValue("Punit");
 inputValue(2417);
--- ProgramTwelve/Union.ts
+++ ProgramTwelve/Union.ts
@@ -1,10 +1,31 @@
 //union in TS is implemented using pipe(|) operation. It is used to combine different values
 
-const inputValue = (userValue: string | number | boolean) => {
-    console.log(userValue);
+// const inputValue = (userValue: string | number | boolean) => {
+//     console.log(userValue);
     
-}
+// }
 
-inputValue("Punit")
-inputValue(2417)
-inputValue(true)
+// inputValue("Punit")
+// inputValue(2417)
+// inputValue(true)
+
+// Practice Question
+
+// const inputValue = (userValue: string | number | boolean) => {
+//     if(typeof userValue === "string"){
+//         console.log(userValue.toUpperCase());        
+//     }
+//     else if(typeof userValue === "number"){
+//         console.log(userValue*2);
+//     }
+//     else if(typeof userValue === "boolean"){
+//         console.log(userValue);
+//     }
+// }
+
+// inputValue("Punit")
+// inputValue(2417)
+// inputValue(true)
+
+
+
