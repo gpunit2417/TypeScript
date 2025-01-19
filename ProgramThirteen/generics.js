5043ef2b5d410a19b7c956e3f874d38e478bad1f
gpunit2417 <punitgoyal106@gmail.com>
2025-01-19 21:19:39 +0530

    practice question on generics

---
+++ ProgramThirteen/generics.js
@@ -0,0 +1,11 @@
+// function logAndReturn<T>(value: T): T {
+//     return value
+// }
+// function add<T>(a:T ,b:T):T ;
+function add(a, b) {
+    return a + b;
+}
+var result1 = add(5, 10);
+var result2 = add("Hello ", "World!");
+console.log(result1);
+console.log(result2);
--- ProgramThirteen/generics.ts
+++ ProgramThirteen/generics.ts
@@ -1,12 +1,26 @@
-function logAndReturn<T>(value: T): T {
-    return value
+// function logAndReturn<T>(value: T): T {
+//     return value
+// }
+
+// const numResult = logAndReturn<number>(23)
+// console.log(numResult);
+
+// const boolResult = logAndReturn<boolean>(true)
+// console.log(boolResult);
+
+// const stringResult = logAndReturn<string>("23")
+// console.log(stringResult);
+
+
+//Practice Question
+
+function add<T>(a:T,b:T):T ;
+function add(a:any,b:any):any {
+     return a+b;
 }
 
-const numResult = logAndReturn<number>(23)
-console.log(numResult);
+const result1 = add<number>(5,10);
+const result2 = add<string>("Hello ","World!");
+console.log(result1);
+console.log(result2);
 
-const boolResult = logAndReturn<boolean>(true)
-console.log(boolResult);
-
-const stringResult = logAndReturn<string>("23")
-console.log(stringResult);
