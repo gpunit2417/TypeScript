05bdcd941107bfc463072c1b97c9123e0ae914f1
gpunit2417 <punitgoyal106@gmail.com>
2025-01-20 13:43:17 +0530

    multiple type variables with generics

---
+++ ProgramFourteen/multipleTypeVar.ts
@@ -0,0 +1,35 @@
+// function add<T>(a:T,b:T):T ;
+// function add(a:any,b:any):any {
+//      return a+b;
+// }
+
+// const result1 = add<number>(5,10);
+// const result2 = add<string>("Hello ","World!");
+// console.log(result1);
+// console.log(result2);
+
+// The above function overloading can be written in a single line using generics
+
+// function add<T>(a:T, b:T): void{
+//     console.log(typeof a);
+//     console.log(typeof b);
+// }
+
+// const one = add(5, 6)
+
+// function add<T, U>(a:T, b:U): void{
+//     console.log(typeof a);
+//     console.log(typeof b);
+// }
+
+// const other = add(5, "Punit")
+
+
+function add1<T, U>(a:T, b:U, c: boolean): void{
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+
+const result = add1("Punit", "goyal", true)
+const resultone = add1<string, string>("Punit", "goyal", true) 
