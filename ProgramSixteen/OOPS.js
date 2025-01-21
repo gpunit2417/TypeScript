1ae26806f1d0acfac09851212150d9bae4943dcb
gpunit2417 <punitgoyal106@gmail.com>
2025-01-21 18:03:25 +0530

    constructor in TS classes

--- ProgramSixteen/OOPS.js
+++ ProgramSixteen/OOPS.js
@@ -1,11 +1,18 @@
 // In TS, a class is blueprint of an object.
+// class Persons {
+//     name: string = "Punit";
+//     age: number = 22;
+//     hobbies: string[] = ["Cricket", "Volleyball"];
+// }
+// const personOneDetail: Persons = new Persons()
+// console.log(personOneDetail);
 var Persons = /** @class */ (function () {
-    function Persons() {
-        this.name = "Punit";
-        this.age = 22;
-        this.hobbies = ["Cricket", "Volleyball"];
+    function Persons(name, age, hobbies) {
+        this.age = age;
+        this.name = name;
+        this.hobbies = hobbies;
     }
     return Persons;
 }());
-var personOneDetail = new Persons();
+var personOneDetail = new Persons("Punit", 22, ["Cricket", "Volleyball"]);
 console.log(personOneDetail);
--- ProgramSixteen/OOPS.ts
+++ ProgramSixteen/OOPS.ts
@@ -1,10 +1,26 @@
 // In TS, a class is blueprint of an object.
 
+// class Persons {
+//     name: string = "Punit";
+//     age: number = 22;
+//     hobbies: string[] = ["Cricket", "Volleyball"];
+// }
+
+// const personOneDetail: Persons = new Persons()
+// console.log(personOneDetail);
+
+
 class Persons {
-    name: string = "Punit";
-    age: number = 22;
-    hobbies: string[] = ["Cricket", "Volleyball"];
+    name: string;
+    age: number;
+    hobbies: string[];
+
+    constructor(name: string, age: number, hobbies: string[]) {
+        this.age = age
+        this.name = name
+        this.hobbies = hobbies
+    }
 }
 
-const personOneDetail: Persons = new Persons()
+const personOneDetail: Persons = new Persons("Punit", 22, ["Cricket", "Volleyball"])
 console.log(personOneDetail);
