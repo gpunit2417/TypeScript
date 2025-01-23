11f44311a5ecec49185a877c85f0a75997417168
gpunit2417 <punitgoyal106@gmail.com>
2025-01-23 21:47:18 +0530

    static keyword in TS

---
+++ ProgramSixteen/staticKeyword.ts
@@ -0,0 +1,11 @@
+// In TS, static methods and properties belong to the class itself rather than to instances of the class. By making methods and properties static, we can access them directly from the class without needing to create an instance of the class. This is useful for utility functions or properties that don't rely on instance specific data.
+
+// class Human{
+//     constructor(public PI: number){}
+// }
+// // @ts-ignore
+// const pi:Human = new Human(3.14);
+// console.log(pi);
+
+
+
