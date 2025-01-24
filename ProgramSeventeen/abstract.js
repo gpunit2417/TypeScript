533d903183fed948f1dadf8d5a96326c5683b33c
gpunit2417 <punitgoyal106@gmail.com>
2025-01-24 18:04:05 +0530

    practice question on abstract done

--- ProgramSeventeen/abstract.js
+++ ProgramSeventeen/abstract.js
@@ -1,6 +1,6 @@
-///* Abstract Classes provide a way to define common properties and methods that  
-// multiple derived classes can share. This promotes code reuse and helps establish a  
-// common interface for related classes.  
+///* Abstract Classes provide a way to define common properties and methods that
+// multiple derived classes can share. This promotes code reuse and helps establish a
+// common interface for related classes.
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -16,57 +16,137 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-///* abstract class cannot be instantiated  
-///* abstract classes focus on class inheritance and sharing common functionality,  
-//* whereas the useContext hook in React focuses on managing global state and allowing  
-// components to consume that state.  
+///* abstract class cannot be instantiated
+///* abstract classes focus on class inheritance and sharing common functionality,
+//* whereas the useContext hook in React focuses on managing global state and allowing
+// components to consume that state.
 // abstract class PerObj {
-//     name: string;  
-//     age: number;  
-// }  
-// class Person: PerObj = {  
-//     name: "vinod",  
-//     age: 29,  
-// };  
-// class Person1: PerObj = {  
-//     name: "thapa",  
-//     age: 29,  
-// };  
-// class Person3: PerObj = {  
-//     name: "thapa",  
-//     age: 29,  
-// };  
-///* Example: Shape Hierarchy  
-///* Suppose you’re building a graphics application, and you want to create a hierarchy  
-// of different shapes. You can use an abstract base class Shape to define common  
-// properties and methods that all shapes share.  
-// circle , rectangle  
-/// SUBSCRIBE TO THAPA TECHNICAL  
-var Shape = /** @class */ (function () {
-    function Shape(color) {
+//     name: string;
+//     age: number;
+// }
+// class Person: PerObj = {
+//     name: "vinod",
+//     age: 29,
+// };
+// class Person1: PerObj = {
+//     name: "thapa",
+//     age: 29,
+// };
+// class Person3: PerObj = {
+//     name: "thapa",
+//     age: 29,
+// };
+///* Example: Shape Hierarchy
+///* Suppose you’re building a graphics application, and you want to create a hierarchy
+// of different shapes. You can use an abstract base class Shape to define common
+// properties and methods that all shapes share.
+// circle , rectangle
+/// SUBSCRIBE TO THAPA TECHNICAL
+// abstract class Shape {
+//     constructor(protected color: string) {}
+//     abstract calculateArea(): number;
+//     abstract displayArea: () => void;
+// }
+// class Circle extends Shape {
+//     constructor(protected color: string, protected radius: number) {
+//         super(color);
+//     }
+//     public calculateArea(): number {
+//         return Math.PI * this.radius * this.radius;
+//     }
+//     displayArea = () => {
+//         console.log(`This is a ${this.color} circle with radius ${this.radius}.`);
+//     };
+// }
+// const circle = new Circle("red", 5);
+// console.log(circle.calculateArea());
+// circle.displayArea();
+///* Practice Time
+///* You need to do the same for Square and Rectangle and if possible use getter and
+// setter methods or static members
+// Area of Rectangle and Square using Getter and Setter Method
+//Area of Square
+var ArSq = /** @class */ (function () {
+    function ArSq(name, color) {
+        this.name = name;
         this.color = color;
     }
-    return Shape;
-}());
-var Circle = /** @class */ (function (_super) {
-    __extends(Circle, _super);
-    function Circle(color, radius) {
-        var _this = _super.call(this, color) || this;
-        _this.color = color;
-        _this.radius = radius;
-        _this.displayArea = function () {
-            console.log("This is a ".concat(_this.color, " circle with radius ").concat(_this.radius, "."));
-        };
-        return _this;
-    }
-    Circle.prototype.calculateArea = function () {
-        return Math.PI * this.radius * this.radius;
+    Object.defineProperty(ArSq.prototype, "side", {
+        get: function () {
+            if (this._side == undefined) {
+                throw new Error("It is undefoned");
+            }
+            return Math.pow(this._side, 2);
+        },
+        set: function (side) {
+            if (side < 0) {
+                throw new Error("Invalid Input");
+            }
+            this._side = side;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    ArSq.prototype.info = function () {
+        return "";
     };
-    return Circle;
-}(Shape));
-var circle = new Circle("red", 5);
-console.log(circle.calculateArea());
-circle.displayArea();
-///* Practice Time  
-///* You need to do the same for Square and Rectangle and if possible use getter and  
-// setter methods or static members  
+    return ArSq;
+}());
+// Area of Rectangle
+var Rectangle = /** @class */ (function () {
+    function Rectangle(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+    Object.defineProperty(Rectangle.prototype, "Length", {
+        // getter method for length
+        get: function () {
+            return this.length;
+        },
+        // setter method for length
+        set: function (value) {
+            if (value < 0) {
+                throw new Error("Length cannot be negative.");
+            }
+            this.length = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Rectangle.prototype, "Width", {
+        // getter method for width
+        get: function () {
+            return this.width;
+        },
+        // setter method for width
+        set: function (value) {
+            if (value < 0) {
+                throw new Error("Width cannot be negative.");
+            }
+            this.width = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // method to calculate area
+    Rectangle.prototype.calculateArea = function () {
+        return this.length * this.width;
+    };
+    return Rectangle;
+}());
+var squ1 = /** @class */ (function (_super) {
+    __extends(squ1, _super);
+    function squ1() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return squ1;
+}(ArSq));
+var rectangle = new Rectangle(10, 50);
+var Ars = new squ1("Square", "Red");
+console.log("Area of rectangle: ".concat(rectangle.calculateArea()));
+// change the length and width
+rectangle.Length = 20;
+rectangle.Width = 10;
+console.log("Area of rectangle after changing length and width: ".concat(rectangle.calculateArea()));
+Ars.side = 50;
+console.log("The Area of Square: ", Ars.side);
--- ProgramSeventeen/abstract.ts
+++ ProgramSeventeen/abstract.ts
@@ -1,68 +1,153 @@
-///* Abstract Classes provide a way to define common properties and methods that  
-// multiple derived classes can share. This promotes code reuse and helps establish a  
-// common interface for related classes.  
+///* Abstract Classes provide a way to define common properties and methods that
+// multiple derived classes can share. This promotes code reuse and helps establish a
+// common interface for related classes.
 
-///* abstract class cannot be instantiated  
+///* abstract class cannot be instantiated
 
-///* abstract classes focus on class inheritance and sharing common functionality,  
-//* whereas the useContext hook in React focuses on managing global state and allowing  
-// components to consume that state.  
+///* abstract classes focus on class inheritance and sharing common functionality,
+//* whereas the useContext hook in React focuses on managing global state and allowing
+// components to consume that state.
 
 // abstract class PerObj {
-//     name: string;  
-//     age: number;  
-// }  
+//     name: string;
+//     age: number;
+// }
 
-// class Person: PerObj = {  
-//     name: "vinod",  
-//     age: 29,  
-// };  
+// class Person: PerObj = {
+//     name: "vinod",
+//     age: 29,
+// };
 
-// class Person1: PerObj = {  
-//     name: "thapa",  
-//     age: 29,  
-// };  
+// class Person1: PerObj = {
+//     name: "thapa",
+//     age: 29,
+// };
 
-// class Person3: PerObj = {  
-//     name: "thapa",  
-//     age: 29,  
-// };  
+// class Person3: PerObj = {
+//     name: "thapa",
+//     age: 29,
+// };
 
-///* Example: Shape Hierarchy  
-///* Suppose you’re building a graphics application, and you want to create a hierarchy  
-// of different shapes. You can use an abstract base class Shape to define common  
-// properties and methods that all shapes share.  
+///* Example: Shape Hierarchy
+///* Suppose you’re building a graphics application, and you want to create a hierarchy
+// of different shapes. You can use an abstract base class Shape to define common
+// properties and methods that all shapes share.
 
-// circle , rectangle  
+// circle , rectangle
 
-/// SUBSCRIBE TO THAPA TECHNICAL  
+/// SUBSCRIBE TO THAPA TECHNICAL
 
-abstract class Shape {  
-    constructor(protected color: string) {}  
+// abstract class Shape {
+//     constructor(protected color: string) {}
 
-    abstract calculateArea(): number;  
-    abstract displayArea: () => void;  
-}  
+//     abstract calculateArea(): number;
+//     abstract displayArea: () => void;
+// }
 
+// class Circle extends Shape {
+//     constructor(protected color: string, protected radius: number) {
+//         super(color);
+//     }
 
-class Circle extends Shape {  
-    constructor(protected color: string, protected radius: number) {  
-        super(color);  
-    }  
+//     public calculateArea(): number {
+//         return Math.PI * this.radius * this.radius;
+//     }
 
-    public calculateArea(): number {  
-        return Math.PI * this.radius * this.radius;  
-    }  
+//     displayArea = () => {
+//         console.log(`This is a ${this.color} circle with radius ${this.radius}.`);
+//     };
+// }
 
-    displayArea = () => {  
-        console.log(`This is a ${this.color} circle with radius ${this.radius}.`);  
-    };  
-}  
+// const circle = new Circle("red", 5);
+// console.log(circle.calculateArea());
+// circle.displayArea();
 
-const circle = new Circle("red", 5);  
-console.log(circle.calculateArea());  
-circle.displayArea();  
+///* Practice Time
+///* You need to do the same for Square and Rectangle and if possible use getter and
+// setter methods or static members
 
-///* Practice Time  
-///* You need to do the same for Square and Rectangle and if possible use getter and  
-// setter methods or static members  
+// Area of Rectangle and Square using Getter and Setter Method
+
+//Area of Square
+class ArSq {
+  private _side: number | undefined;
+  constructor(public name: string, public color: string) {}
+
+  public set side(side: number) {
+    if (side < 0) {
+      throw new Error("Invalid Input");
+    }
+
+    this._side = side;
+  }
+
+  public get side() {
+    if (this._side == undefined) {
+      throw new Error("It is undefined");
+    }
+
+    return this._side ** 2;
+  }
+
+  info(): string {
+    return ``;
+  }
+}
+
+// Area of Rectangle
+class Rectangle {
+  private length: number;
+  private width: number;
+
+  constructor(length: number, width: number) {
+    this.length = length;
+    this.width = width;
+  }
+
+  // getter method for length
+  get Length(): number {
+    return this.length;
+  }
+
+  // setter method for length
+  set Length(value: number) {
+    if (value < 0) {
+      throw new Error("Length cannot be negative.");
+    }
+    this.length = value;
+  }
+
+  // getter method for width
+  get Width(): number {
+    return this.width;
+  }
+
+  // setter method for width
+  set Width(value: number) {
+    if (value < 0) {
+      throw new Error("Width cannot be negative.");
+    }
+    this.width = value;
+  }
+
+  // method to calculate area
+  calculateArea(): number {
+    return this.length * this.width;
+  }
+}
+
+class squ1 extends ArSq {}
+
+const rectangle = new Rectangle(10, 50);
+const Ars: ArSq = new squ1("Square", "Red");
+console.log(`Area of rectangle: ${rectangle.calculateArea()}`);
+
+// change the length and width
+rectangle.Length = 20;
+rectangle.Width = 10;
+console.log(
+  `Area of rectangle after changing length and width: ${rectangle.calculateArea()}`
+);
+
+Ars.side = 50;
+console.log("The Area of Square: ", Ars.side);
